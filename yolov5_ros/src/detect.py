c060480ef754758695da0df6f3759117cf903ea6
Shiju Quan <shiju.quan@outlook.com>
2023-06-26 18:30:01 +0200

    basic_depth

--- yolov5_ros/src/detect.py
+++ yolov5_ros/src/detect.py
@@ -5,12 +5,12 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 import numpy as np
-from cv_bridge import CvBridge
-from pathlib import Path
 import os
 import sys
+import random
+from cv_bridge import CvBridge
+from pathlib import Path
 from rostopic import get_topic_type
-
 from sensor_msgs.msg import Image, CompressedImage
 from detection_msgs.msg import BoundingBox, BoundingBoxes
 
@@ -75,8 +75,10 @@ class Yolov5Detector:
         self.model.warmup()  # warmup        
         
         # Initialize subscriber to Image/CompressedImage topic
+        # 初始化订阅输入图像话题，获取topic type， topic name
         input_image_type, input_image_topic, _ = get_topic_type(rospy.get_param("~input_image_topic"), blocking = True)
-        self.compressed_input = input_image_type == "sensor_msgs/CompressedImage"
+        #判断这个input_image_type是不是compressedimage类型的消息
+        self.compressed_input = input_image_type == "sensor_msgs/CompressedImage" 
 
         if self.compressed_input:
             self.image_sub = rospy.Subscriber(
@@ -87,7 +89,16 @@ class Yolov5Detector:
                 input_image_topic, Image, self.callback, queue_size=1
             )
 
-        # Initialize prediction publisher
+        #Initialize subscriber to depth image topic 初始化订阅深度图像，
+        #此处LAUNCH文件需要修改
+        input_depth_type, input_depth_topic, _ = get_topic_type(rospy.get_param("~depth_image_topic"), blocking = True)
+        print(input_depth_type)
+        self.depth_image_sub = rospy.Subscriber(
+            input_depth_topic, Image, self.depth_callback, queue_size=1
+        )
+
+
+        # Initialize prediction publisher 建立预测的话题发布，消息类型为boundingboxes
         self.pred_pub = rospy.Publisher(
             rospy.get_param("~output_topic"), BoundingBoxes, queue_size=10
         )
@@ -95,11 +106,65 @@ class Yolov5Detector:
         self.publish_image = rospy.get_param("~publish_image")
         if self.publish_image:
             self.image_pub = rospy.Publisher(
-                rospy.get_param("~output_image_topic"), Image, queue_size=10
+                rospy.get_param("~depth_output_topic"), Image, queue_size=10
             )
+        
+        """ 如果已经存在相机的深度图publisher应该不需要写这一部分，不确定，可能也需要一个publisher发布带框的深度图
+        #Initialize depth publisher
+        self.publish_depth_image = rospy.get_param("~publish_depth_image")
+        if self.publish_depth_image:
+            self.depth_pub = rospy.Publisher(
+                rospy.get_param("~output_depth_image"), Image, queue_size=10
+            )
+        """
         
         # Initialize CV_Bridge
         self.bridge = CvBridge()
+        self.color_image = None
+        self.depth_image = None
+        
+     #深度callback函数
+    def depth_callback(self, depth_image):
+        try:
+            # Convert depth image to numpy array
+            cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image, desired_encoding='passthrough')
+            self.depth_image = np.array(cv_depth_image, dtype=np.float64)
+            self.depth_image = np.nan_to_num(self.depth_image)  # Replace NaN values with 0
+        except CvBridgeError as e:
+            rospy.logerr('Error converting depth image: {}'.format(e))
+            return     
+
+    def filter(self, x, y, min_val, randnum):
+        distance_list = []
+        #mid_pos = [(box[0] + box[2])//2, (box[1] + box[3])//2] #确定索引深度的中心像素位置
+        #min_val = min(abs(box[2] - box[0]), abs(box[3] - box[1])) #确定深度搜索范围
+        #print(box)
+        for i in range(randnum):
+            bias = random.randint(-min_val//4, min_val//4)
+            dist = self.depth_image[int(y + bias), int(x + bias)]
+            if dist:
+                distance_list.append(dist)
+        distance_list = np.array(distance_list)
+        distance_list = np.sort(distance_list)[randnum//2-randnum//4:randnum//2+randnum//4] #冒泡排序+中值滤波
+    #print(distance_list, np.mean(distance_list))
+        distance = np.mean(distance_list)
+        return distance
+
+
+    def preprocess(self, img):
+        """
+        Adapted from yolov5/utils/datasets.py LoadStreams class
+        """
+        img0 = img.copy()
+        #取letterbox返回的第一个值，即img，转换成np数组
+        img = np.array([letterbox(img, self.img_size, stride=self.stride, auto=self.pt)[0]]) 
+        # Convert
+        img = img[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW
+        #重新构造一个连续数组img
+        img = np.ascontiguousarray(img)
+
+        return img, img0 
+    
 
     def callback(self, data):
         """adapted from yolov5/detect.py"""
@@ -138,12 +203,25 @@ class Yolov5Detector:
         annotator = Annotator(im0, line_width=self.line_thickness, example=str(self.names))
         if len(det):
             # Rescale boxes from img_size to im0 size
+            # 将Boundingboxes 恢复到原始图像im0的尺寸
             det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
             # Write results
             for *xyxy, conf, cls in reversed(det):
                 bounding_box = BoundingBox()
                 c = int(cls)
+
+                # Calculate the center of the bounding box
+                x_center = int((xyxy[0] + xyxy[2]) / 2)
+                y_center = int((xyxy[1] + xyxy[3]) / 2)
+
+                min_val = min(abs(xyxy[2] - xyxy[0]), abs(xyxy[3] - xyxy[1]))
+
+                # Add depth i   nformation to the bounding box
+                if self.depth_image is not None:
+                    distance_bbc = self.filter(x_center, y_center, min_val, 24)
+                    bounding_box.distance = distance_bbc
+
                 # Fill in bounding box message
                 bounding_box.Class = self.names[c]
                 bounding_box.probability = conf 
@@ -152,13 +230,14 @@ class Yolov5Detector:
                 bounding_box.xmax = int(xyxy[2])
                 bounding_box.ymax = int(xyxy[3])
 
+                #
                 bounding_boxes.bounding_boxes.append(bounding_box)
 
                 # Annotate the image
                 if self.publish_image or self.view_image:  # Add bbox to image
                       # integer class
                     label = f"{self.names[c]} {conf:.2f}"
-                    annotator.box_label(xyxy, label, color=colors(c, True))       
+                    annotator.box_label(xyxy, label, color=colors(c, True))     
 
                 
                 ### POPULATE THE DETECTION MESSAGE HERE
@@ -175,20 +254,9 @@ class Yolov5Detector:
             cv2.waitKey(1)  # 1 millisecond
         if self.publish_image:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(im0, "bgr8"))
+            self.color_image = im0            
+    
         
-
-    def preprocess(self, img):
-        """
-        Adapted from yolov5/utils/datasets.py LoadStreams class
-        """
-        img0 = img.copy()
-        img = np.array([letterbox(img, self.img_size, stride=self.stride, auto=self.pt)[0]])
-        # Convert
-        img = img[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW
-        img = np.ascontiguousarray(img)
-
-        return img, img0 
-
 
 if __name__ == "__main__":
 
